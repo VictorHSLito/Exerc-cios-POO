e0b01b3e12d332fd51d266c4586a85b3989cde0f
Victor Hugo <victorferrarihugo@hotmail.com>
2024-08-23 21:07:03 -0300

    Exercícios

---
+++ Ex01.java
@@ -0,0 +1,20 @@
+//Elabore um programa que leia 5 valores e calcule a média aritmética deles. Ao final do programa, apresente os valores digitados e a média aritmética.
+
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int size = 5;
+        float [] notas = new float[size];
+        float media = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite 5 notas: ");
+
+        for (int i = 0; i < size; ++i) {
+            notas[i] = sc.nextFloat();
+            media += notas[i];
+        }
+        media = (media)/5;
+        System.out.println("A media final vale: " + media);
+    }
+}
---
+++ Ex02.java
@@ -0,0 +1,15 @@
+/*Faça um programa para ler o valor de um círculo, e depois mostre o valor da área deste círculo com quatro casas decimais.
+Você deverá utilizar o JOptionPane. */
+import javax.swing.JOptionPane;
+public class Ex02 {
+    public static void main (String[] args) {
+        String raio = JOptionPane.showInputDialog("Digite o valor do raio do círculo: ");
+        float PI = 3.14159f;
+
+        float raio1 = Float.parseFloat(raio);
+        float area = PI*(raio1*raio1);
+
+        String mensagem = String.format("A área do circulo de raio %s, vale: %.4f", raio, area);
+        JOptionPane.showMessageDialog(null, mensagem);
+    }
+}
