abfb66f1ae221144a1fb1e658cb05182e1e3d66e
Victor Hugo <victorferrarihugo@hotmail.com>
2024-09-03 14:31:43 -0300

    Exercício 3

---
+++ Atividade 3/src/Exercício03/Principal.java
@@ -0,0 +1,44 @@
+package Exercício03;
+
+public class Principal {
+    public static void main(String[] args) {
+        Triangulo t1 = new Triangulo();
+        Triangulo t2 = new Triangulo();
+        boolean teste = false;
+        System.out.println("Digite os lados do primeiro triangulo: ");
+        t1.lerLados();
+        teste = t1.verificaTriangulo();
+
+        if (teste == false) {
+            do {
+                System.out.println("Digite novamente os lados: ");
+                t1.lerLados();
+                teste = t1.verificaTriangulo();
+            } while (teste != true);
+        }
+        System.out.println("Agora digite os lados do segundo triangulo: ");
+        t2.lerLados();
+        teste = t2.verificaTriangulo();
+
+        if (teste == false) {
+            do {
+                System.out.println("Digite novamente os lados: ");
+                t2.lerLados();
+                teste = t2.verificaTriangulo();
+            } while (teste != true);
+        }
+
+        t1.area = t1.calculaArea(t1.calculaSemiPerimetro());
+        t2.area = t2.calculaArea(t2.calculaSemiPerimetro());
+
+        System.out.printf("Área do primeiro triângulo: %.2f\n", t1.area);
+        System.out.printf("Área do segundo triângulo: %.2f\n", t2.area);
+
+        if (t1.area > t2.area) {
+            System.out.println("O triângulo com maior área é o primeiro triângulo");
+        }
+        else {
+            System.out.println("O triângulo com maior área é o segundo triângulo");
+        }
+    }
+}
---
+++ Atividade 3/src/Exercício03/Triangulo.java
@@ -0,0 +1,49 @@
+package Exercício03;
+
+import java.util.Scanner;
+
+public class Triangulo {
+    int [] triangulo = new int[3];
+    double area;
+
+    Scanner sc = new Scanner(System.in);
+
+
+    public void lerLados() {
+        for (int i = 0; i < 3; i++) {
+            this.triangulo[i] = sc.nextInt();
+            if (this.triangulo[i] < 0) {
+                System.out.println("Nâo é possível existir lado negativo!");
+                break;
+            }
+        }
+    }
+
+    public boolean verificaTriangulo() {
+        if ((this.triangulo[0] + this.triangulo[1] > this.triangulo[2]) &&
+                (this.triangulo[1] + this.triangulo[2] > this.triangulo[0]) &&
+                (this.triangulo[2] + this.triangulo[0] > this.triangulo[1])) {
+            System.out.println("Triângulo válido");
+            return true;
+        }
+        else {
+            System.out.println("Triângulo inválido!");
+            return false;
+        }
+    }
+
+    public float calculaSemiPerimetro() {
+        float perimetro = 0;
+        for (int i = 0; i < 3; i++) {
+            perimetro += this.triangulo[i];
+        }
+        return perimetro/2;
+    }
+
+    public double calculaArea(float perimetro) {
+        double area = Math.sqrt(perimetro*(perimetro - this.triangulo[0])*
+                (perimetro - this.triangulo[1])*(perimetro-this.triangulo[2]));
+        return area;
+    }
+
+}
